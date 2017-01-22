cc114495674c7431b935b3a42e380f9b1336a41e
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2017-01-22 17:20:41 -0500

    Understanding lifecycle components

--- Desktop/first-app/src/app/lifecyle.component.ts
+++ Desktop/first-app/src/app/lifecyle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'fa-lifecyle',
@@ -9,11 +9,44 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: []
 })
-export class LifecyleComponent implements OnInit {
+export class LifecyleComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   constructor() { }
 
+  ngOnChanges() {
+    this.log('ngOnChanges')
+  }
+
   ngOnInit() {
+    this.log('ngOnInit')
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck')
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked')
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit')
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked')
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy')
+  }
+
+  private log (hook: string) {
+      console.log(hook);
   }
 
 }
